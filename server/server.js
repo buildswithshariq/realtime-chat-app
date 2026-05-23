e35d4185747a9cbc95a4288f9dd1505fcae19aaa
Shariq Khan <shariqkhann488@gmail.com>
2026-05-23 11:10:47 +0000

    MongoDB Connection Added

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -11,6 +13,8 @@ app.get("/", (req, res) => {
 });
 
 const PORT = 5000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
